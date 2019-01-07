4fd9bf4b0cc18bbc71c54ba969c88dd4b2dc9215
Chris Green <chris.green@toptal.com>
2019-01-07 13:46:26 -0500

    complete solution including credentials in appsettings.js

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,83 @@
+﻿using System.Dynamic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Okta.Sdk;
+using OktaAspNetCoreMvc.Models;
+
+namespace OktaAspNetCoreMvc.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IOktaClient _oktaClient;
+
+        public AccountController(IOktaClient oktaClient = null)
+        {
+            _oktaClient = oktaClient;
+        }
+
+        public IActionResult Login()
+        {
+            if (!HttpContext.User.Identity.IsAuthenticated)
+            {
+                return Challenge(OpenIdConnectDefaults.AuthenticationScheme);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        public IActionResult Logout()
+        {
+            if (HttpContext.User.Identity.IsAuthenticated)
+            {
+                return SignOut(CookieAuthenticationDefaults.AuthenticationScheme, OpenIdConnectDefaults.AuthenticationScheme);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }    
+     
+        [Authorize]
+        public IActionResult Claims()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Me()
+        {
+            var username = User.Claims
+                .FirstOrDefault(x => x.Type == "preferred_username")
+                ?.Value.ToString();
+
+            var viewModel = new MeViewModel
+            {
+                Username = username,
+                SdkAvailable = _oktaClient != null
+            };
+
+            if (!viewModel.SdkAvailable)
+            {
+                return View(viewModel);
+            }
+
+            if (!string.IsNullOrEmpty(username))
+            {
+                var user = await _oktaClient.Users.GetUserAsync(username);
+                dynamic userInfoWrapper = new ExpandoObject();
+                userInfoWrapper.Profile = user.Profile;
+                userInfoWrapper.PasswordChanged = user.PasswordChanged;
+                userInfoWrapper.LastLogin = user.LastLogin;
+                userInfoWrapper.Status = user.Status.ToString();
+                viewModel.UserInfo = userInfoWrapper;
+
+                viewModel.Groups = (await user.Groups.ToList()).Select(g => g.Profile.Name).ToArray();
+            }
+            
+            return View(viewModel);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Okta.Sdk;
+using Okta.Sdk.Configuration;
+
+namespace OktaAspNetCoreMvc
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(sharedOptions =>
+            {
+                sharedOptions.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                sharedOptions.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                sharedOptions.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            })
+            .AddCookie()
+            .AddOpenIdConnect(options =>
+            {
+                // Configuration pulled from appsettings.json by default:
+                options.ClientId = Configuration["okta:ClientId"];
+                options.ClientSecret = Configuration["okta:ClientSecret"];
+                options.Authority = Configuration["okta:Issuer"];
+                options.CallbackPath = "/authorization-code/callback";
+                options.ResponseType = "code";
+                options.SaveTokens = true;
+                options.UseTokenLifetime = false;
+                options.GetClaimsFromUserInfoEndpoint = true;
+                options.Scope.Add("openid");
+                options.Scope.Add("profile");
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    NameClaimType = "name"
+                };
+            });
+            
+            if (!string.IsNullOrEmpty(Configuration["okta:OrgUrl"]) && !string.IsNullOrEmpty(Configuration["okta:APIToken"]))
+            {
+                services.AddSingleton<IOktaClient>
+                (
+                    new OktaClient(new OktaClientConfiguration()
+                    {
+                        OrgUrl = Configuration["okta:OrgUrl"],
+                        Token = Configuration["okta:APIToken"]
+                    })
+                );
+            }
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
