a789643fcae7ba8b372f24ef0108576270254cf1
Chris Green <chris.green@toptal.com>
2019-01-21 16:37:58 -0500

    cleaned up code; removed unneeded files and unneeded lines of code

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Mvc;
+using Okta.Sdk;
+
+namespace OktaAspNetCoreMvc.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IOktaClient _oktaClient;
+
+        public AccountController(IOktaClient oktaClient = null)
+        {
+            _oktaClient = oktaClient;
+        }
+
+        public IActionResult Login()
+        {
+            if (!HttpContext.User.Identity.IsAuthenticated)
+            {
+                return Challenge(OpenIdConnectDefaults.AuthenticationScheme);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Models/MeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OktaAspNetCoreMvc.Models
+{
+    public class MeViewModel
+    {
+        public string Username { get; set; }
+        public dynamic Profile { get; set; }
+        public DateTimeOffset? PasswordChanged { get; set; }
+        public DateTimeOffset? LastLogin { get; set; }
+        public string Status { get; set; }
+    }
+}
